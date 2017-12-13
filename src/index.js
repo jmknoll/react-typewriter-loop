f3ee7050be1e8212c58fe5baa9d94881f4803edd
Jameson Knoll <j.knoll621@gmail.com>
2017-12-14 02:24:32 +0800

    initial setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ReactTypewriterLoop extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This will be the react typewriter</p>
+        <p>And it is watching in real time</p>
+      </div>
+    );
+  }
+}
+export default ReactTypewriterLoop;
