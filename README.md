da044d36175474c2298dd296771b75f1e021c9e2
Jameson Knoll <j.knoll621@gmail.com>
2017-12-14 14:30:11 +0800

    implement default prop types and prop validations

---
+++ README.md
@@ -0,0 +1,12 @@
+//todo
+
+1. get basic implementation working
+2. write tests
+3. continue to iterate on this and create a more robust component
+4. write documentation
+5. create example page
+6. deploy to github pages
+7. publish to npm
+8. complete documentation
+8. write blog post about how to do this whole process
+9. plan on working on this most nights over the next week to really get a solid understanding of the whole process
--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class ReactTypewriterLoop extends React.Component {
+class TypewriterLoop extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.renderTypedStrings = this.renderTypedStrings.bind(this);
+  }
+
+  renderTypedStrings() {
+    const { typedStrings, delay } = this.props
+    return <h1>{typedStrings[0]}</h1>
+  }
+
+
   render() {
     return (
       <div>
-        <p>This will be the react typewriter</p>
-        <p>And it is watching in real time</p>
+        {this.renderTypedStrings()}
       </div>
     );
   }
 }
-export default ReactTypewriterLoop;
+
+export default TypewriterLoop;
+
+/* props
+
+typedStrings: array of strings to type out
+typeSpeed: a number to indicate typing speed, 0 - 100, 100 is instant
+deleteSpeed: a number to indicate deletion speed, 0 - 100, 100 is instant
+delay: delay after typing each string, in ms
+loop: loop over items or just run through them once
+cursor: boolean to indicate whether cursor should show
+
+*/
+
+TypewriterLoop.defaultProps = {
+  typedStrings: ['string1', 'string2'],
+  typeSpeed: 50,
+  deleteSpeed: 50,
+  delay: 1000,
+  loop: true,
+  cursor: true
+};
+
+TypewriterLoop.PropTypes = {
+  typedStrings: PropTypes.arrayOf(PropTypes.string),
+  typeSpeed: PropTypes.number,
+  deleteSpeed: PropTypes.number,
+  delay: PropTypes.number,
+  loop: PropTypes.bool,
+  cursor: PropTypes.bool
+};
